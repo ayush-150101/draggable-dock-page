cbb5cd54a06b44ed1756eaf16b47ba7039d68524
ayush-150101 <57745707+ayush-150101@users.noreply.github.com>
2024-11-08 11:38:22 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+/// Entrypoint of the application.
+void main() {
+  runApp(const MyApp());
+}
+
+/// [Widget] building the [MaterialApp].
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            //width: 500,
+            //constraints: const BoxConstraints(maxWidth: 350),
+            height: 70,
+            child: Center(
+              child: Dock(
+                items: const [
+                  Icons.person,
+                  Icons.message,
+                  Icons.call,
+                  Icons.camera,
+                  Icons.photo,
+                ],
+                builder: (e) {
+                  return Container(
+                    constraints: const BoxConstraints(minWidth: 48),
+                    height: 48,
+                    margin: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors
+                          .primaries[e.hashCode % Colors.primaries.length],
+                    ),
+                    child: Center(child: Icon(e, color: Colors.white)),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Dock of the reorderable [items].
+class Dock<T> extends StatefulWidget {
+  const Dock({
+    super.key,
+    this.items = const [],
+    required this.builder,
+  });
+
+  /// Initial [T] items to put in this [Dock].
+  final List<T> items;
+
+  /// Builder building the provided [T] item.
+  final Widget Function(T) builder;
+
+  @override
+  State<Dock<T>> createState() => _DockState<T>();
+}
+
+/// State of the [Dock] used to manipulate the [_items].
+class _DockState<T> extends State<Dock<T>> {
+  /// [T] items being manipulated.
+  late final List<T> _items = widget.items.toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 328,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.black12,
+      ),
+      padding: const EdgeInsets.all(4),
+      child: ReorderableListView(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: false,
+
+        /// Set to false to build custom draggable element
+        buildDefaultDragHandles: false,
+
+        /// Set the items in corresponding order once reordering stops
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            if (newIndex > oldIndex) newIndex -= 1;
+            final item = _items.removeAt(oldIndex);
+            _items.insert(newIndex, item);
+          });
+        },
+        children: _items.asMap().entries.map((entry) {
+          final index = entry.key;
+          final item = entry.value;
+
+          return ReorderableDragStartListener(
+              index: index,
+              key: Key(entry.key.toString()),
+              child: widget.builder(item));
+        }).toList(),
+      ),
+    );
+  }
+}
